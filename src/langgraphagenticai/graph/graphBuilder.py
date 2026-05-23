f3d5d9ed1dd2c21006b2982897e1c2210a94a3c7
satvik mittal <satvik190603@gmail.com>
2026-05-23 07:00:40 -0700

    created a pipeline to connect frontend with backend

--- src/langgraphagenticai/graph/graphBuilder.py
+++ src/langgraphagenticai/graph/graphBuilder.py
@@ -1,5 +1,6 @@
 from langgraph.graph import StateGraph, END, START
 from src.langgraphagenticai.state.state import state
+from src.langgraphagenticai.nodes.basicChatbotNode import basicChatbotNode
 
 class GraphBuilder:
     def __init__(self,model):
@@ -8,6 +9,17 @@ class GraphBuilder:
 
     def basic_chatbot_build_graph(self):
 
-        self.graph_builder.add_node("chatBot","")
-        self.graph_builder.add_edge(START,"chatbot")
-        self.graph_builder.add_edge("chatbot",END)
+        self.basic_chatbot_node = basicChatbotNode(self.llm)
+
+        self.graph_builder.add_node("chatBot",self.basic_chatbot_node.process)
+        self.graph_builder.add_edge(START,"chatBot")
+        self.graph_builder.add_edge("chatBot",END)
+
+        return self.graph_builder.compile()
+
+
+    def set_use_case(self,usecase):
+        if usecase == "basic_chatbot":
+            return self.basic_chatbot_build_graph()
+        else:
+            raise ValueError("Invalid use case")
--- src/langgraphagenticai/main.py
+++ src/langgraphagenticai/main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from src.langgraphagenticai.frontend.streamLitUi.loadUi import LoadUi
+from src.langgraphagenticai.llms.groqLLM import GroqLLM
+from src.langgraphagenticai.graph.graphBuilder import GraphBuilder
+from src.langgraphagenticai.frontend.streamLitUi.displayResult import DisplayResultStreamlit
 
 def load_langgrap_Agentic_api():
 
@@ -13,6 +16,37 @@ def load_langgrap_Agentic_api():
 
     user_message = st.chat_input("Enter your message")
 
+    if user_message:
+        try:
+            obj_llm_config = GroqLLM(user_controls_input=userControls)
+            llm_model = obj_llm_config.get_llm_model()
+            if not llm_model:
+                st.error("Error: LLM model could not be initialised")
+                return
+            
+            usecase = userControls.get("usecase")
+
+            if not usecase:
+                st.error("Error: Use case is not selected")
+                return
+            
+
+            graph_builder = GraphBuilder(llm_model)
+            try:
+                graph = graph_builder.set_use_case(usecase)
+                display = DisplayResultStreamlit(usecase,graph,user_message)
+                display.display_result()
+                if not graph:
+                    st.error("Error: Graph could not be initialised")
+                    return
+                
+            except Exception as e:
+                st.error(f"Error: {e}")
+                return
+                
+        except Exception as e:
+            st.error(f"Error: {e}")
+            return
 
 
     
